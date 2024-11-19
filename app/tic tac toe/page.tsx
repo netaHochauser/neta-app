41ce571382e151813e3a10d60136fbc8600348ed
netahoch <neta.hochauser@mail.huji.ac.il>
2024-11-19 18:23:48 +0200

    tic tac toe

--- app/tic tac toe/page.tsx
+++ app/tic tac toe/page.tsx
@@ -19,17 +19,38 @@ function Square({ value, onSquareClick }: SquareProps) {
 
 export default function Board() {
   // State type: An array of 9 elements, each being either a string or null
+  const [xIsNext, setIsNext] = useState<boolean>(true);
   const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null));
-
+  
   // Type annotation for the handleClick function
   function handleClick(i: number): void {
+    if (squares[i] || calculateWinner(squares)){
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X'; // Update the clicked square with "X"
+    if( xIsNext){
+      nextSquares[i] = 'X'; // Update the clicked square with "X"
+    }
+    else{
+      nextSquares[i] = 'O'; // Update the clicked square with "O"
+    }
     setSquares(nextSquares);
+    setIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = "Winner: " + winner;
+  }
+  else{
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
     <>
+      <div className={styles.status}>{status}</div>
       <div className={styles.boardRow}>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -47,4 +68,26 @@ export default function Board() {
       </div>
     </>
   );
+
+  function calculateWinner(squares : (string | null)[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
+
+
